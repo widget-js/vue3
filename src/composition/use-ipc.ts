72c8bc902f0192217a0ab36d1b631a105943b5f7
Neo Fu <rtugeek@gmail.com>
2022-12-28 17:33:00 +0800

    add useKeyboardEventHook useMouseEventHook

--- src/composition/use-ipc.ts
+++ src/composition/use-ipc.ts
@@ -1,4 +1,4 @@
-import {Channel, ElectronApi} from "@widget-js/core";
+import {AppNotification, AppKeyboardEvent, AppMouseEvent, Channel, ElectronApi} from "@widget-js/core";
 import {onMounted, onUnmounted} from "vue";
 
 export function useIpcListener(channel: Channel, callback: (...args) => void) {
@@ -11,5 +11,40 @@ export function useIpcListener(channel: Channel, callback: (...args) => void) {
     onUnmounted(async () => {
         await ElectronApi.removeIpcListener(channel);
     });
-
 }
+
+export function useNotification(callback: (notification?: AppNotification) => void) {
+    onMounted(async () => {
+        await ElectronApi.addIpcListener(Channel.NOTIFICATION, (notification?: AppNotification) => {
+            callback(notification);
+        });
+    });
+    onUnmounted(async () => {
+        await ElectronApi.removeIpcListener(Channel.NOTIFICATION);
+    });
+}
+
+
+export function useMouseEventHook(callback: (event: AppMouseEvent) => void) {
+    onMounted(async () => {
+        await ElectronApi.addIpcListener(Channel.MOUSE_EVENT, (event: AppMouseEvent) => {
+            callback(event);
+        });
+    });
+    onUnmounted(async () => {
+        await ElectronApi.removeIpcListener(Channel.MOUSE_EVENT);
+    });
+}
+
+
+export function useKeyboardEventHook(callback: (event: AppKeyboardEvent) => void) {
+    onMounted(async () => {
+        await ElectronApi.addIpcListener(Channel.KEYBOARD_EVENT, (event: AppKeyboardEvent) => {
+            callback(event);
+        });
+    });
+    onUnmounted(async () => {
+        await ElectronApi.removeIpcListener(Channel.KEYBOARD_EVENT);
+    });
+}
+
